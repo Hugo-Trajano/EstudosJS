a6eee9fb3b8cb90f2f9759f1da3cfd83ced05095
Hugo-Trajano <hugo.trajano@a.ficr.edu.br>
2021-01-23 16:03:50 -0300

    Continuando os Estudos

---
+++ aula 11/ex.008.js
@@ -0,0 +1,10 @@
+var vel = 40
+console.log(` A velocidade do seu carro é ${vel}`)
+if (vel > 60){
+    console.log('MULTADO')
+}
+else{
+    console.log('Dirija bem com cinto de segurança')
+
+}
+
---
+++ aula 12/exe.11.js
@@ -0,0 +1,9 @@
+var idade = 10
+
+if (idade < 16){
+    console.log('Não vota')
+}else if (idade < 18){
+    console.log('Voto Opicional')
+}else {
+    console.log('Voto obrigatorio')
+}
