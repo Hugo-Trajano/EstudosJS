da52af2c0ac0874f779a9223874ba85f12c64277
Hugo-Trajano <hugo.trajano@a.ficr.edu.br>
2021-01-24 21:50:02 -0300

    Terminando primeiro Exercicio

---
+++ aula 12 ex/ex 14/script.js
@@ -0,0 +1,21 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas!`
+    // forçar variavel => var hora = 10
+    if  (hora >= 0 && hora < 12){
+        //Bom dia
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#ffffcc'
+    }else if (hora >= 12 && hora < 18){
+        //Boa tarde
+        img.src = 'fototarde.png'
+        document.body.style.background = '#d9ffb3'
+    }else {
+        //Boa noite
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#000033'
+    }
+}
